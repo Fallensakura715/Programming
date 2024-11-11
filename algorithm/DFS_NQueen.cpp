abc5816e51bd9753c63df3bc41e20f5afb8dcf96
陈佳显 <fallensakura715@gmail.com>
2024-11-11 17:12:40 +0800

    Update DFS_NQueen.cpp

--- algorithm/DFS_NQueen.cpp
+++ algorithm/DFS_NQueen.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 using namespace std;
-#define N 100
+#define N 20
 
 int n;
 char path[N][N];
 bool dg[N],udg[N],col[N];
-//全排列解法
+//全排列解法 u是row
 void dfs(int u) {
-    if (n == u) {
+    if (n==u) {
         for (int i = 0; i < n; ++i) {
             puts(path[i]);
         }
@@ -15,12 +15,12 @@ void dfs(int u) {
         return;
     }
     for (int i = 1; i <= n; ++i) {
-        //对角线下标想象成一次函数的截距，row和col想象成x和y  +n是防止<0
-        if (!dg[u-i+n] && !udg[i+u] && !col[i]) {
-            path[u][i] = 'Q';
+        //对角线下标想象成一次函数的截距，row和col想象成x和y  +n是防止(u-i)<0
+        if (!dg[u-i+u] && !udg[i+u] && !col[i]) {
+            path[u][i]= 'Q';
             col[i]=dg[u-i+n]=udg[u+i]= true;
-            dfs(u + 1);
-            path[u][i] = '.';
+            dfs(u+1);
+            path[u][i]= '.';
             col[i]=dg[u-i+n]=udg[u+i]= false;
         }
     }
@@ -34,5 +34,49 @@ int main(){
         }
     }
     dfs(0);
+    return 0;
+}
+
+//原始解法
+#include <iostream>
+using namespace std;
+#define N 20
+
+int n;
+char path[N][N];
+bool row[N],col[N],dg[N],udg[N];
+
+void dfs(int x,int y,int queen) {
+    if (y==n) x++,y=0;
+    if (x==n) {
+        if (queen==n) {
+            for (int i = 0; i < n; ++i) {
+                puts(path[i]);
+            }
+            cout<<endl;
+        }
+        return;
+    }
+    //不放皇后
+    dfs(x,y+1,queen);
+    //放皇后
+    if (!dg[x-y+n] && !udg[x+y] && !row[x] && !col[y]) {
+        dg[x-y+n]=udg[x+y]=row[x]=col[y]= true;
+        path[x][y]='Q';
+        dfs(x,y+1,queen+1);
+        dg[x-y+n]=udg[x+y]=row[x]=col[y]= false;
+        path[x][y]='.';
+    }
+}
+
+
+int main(){
+    cin>>n;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            path[i][j]='.';
+        }
+    }
+    dfs(0,0,0);
     return 0;
 }
