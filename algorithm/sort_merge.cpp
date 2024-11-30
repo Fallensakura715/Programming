e6be3c59ae6b23e3a365a4b82b7ce722b26f92ae
Fallensakura <fallensakura715@gmail.com>
2024-11-30 12:15:29 +0800

    Update sort_merge.cpp

--- algorithm/sort_merge.cpp
+++ algorithm/sort_merge.cpp
@@ -32,9 +32,8 @@ int main()
         scanf("%d", &arr[i]);
     }
     merge_sort(arr, 0, n-1);
-
     for (int i = 0; i < n; ++i) {
-        printf("%d",arr[i]);
+        printf("%d", arr[i]);
     }
     return 0;
 }
