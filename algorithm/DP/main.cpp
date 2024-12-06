42528975fbb4386f8820706f89332b79ecf0e818
Fallensakura <fallensakura715@gmail.com>
2024-12-06 17:59:38 +0800

    Add files via upload

---
+++ algorithm/DP/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+constexpr int N = 1010;
+int n, capacity;
+int v[N], w[N], dp[N], dp2[N][N];
+
+int main() {
+    cin >> n >> capacity;
+    for (int i = 1; i <= n; ++i) {
+        cin >> v[i] >> w[i];
+    }
+
+    //二维dp
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 0; j <= capacity; ++j) {
+            dp2[i][j] = dp2[i - 1][j];
+            if (j >= v[i]) dp2[i][j] = max(dp2[i][j], dp2[i][j - v[i]] + w[i]);
+        }
+    }
+    
+    //一维优化版
+    for (int i = 1; i <= n; ++i) {
+        for (int j = v[i]; j <= capacity; ++j) {
+            dp[j] = max(dp[j], dp[j - v[i]] + w[i]);
+        }
+    }
+
+    cout << dp[capacity] << endl;
+    cout << dp2[n][capacity] << endl;
+    return 0;
+}
