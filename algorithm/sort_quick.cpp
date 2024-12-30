0bd0953739825e5c85b2dcb216610033c3babc7c
Fallensakura <fallensakura715@gmail.com>
2024-12-30 14:15:22 +0800

    Update sort_quick.cpp

--- algorithm/sort_quick.cpp
+++ algorithm/sort_quick.cpp
@@ -21,6 +21,29 @@ void quick_sort(int arr[],int l,int r){
     quick_sort(arr,j+1,r);
 }
 
+int partition(std::vector<int>& arr, int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+void quickSort(int &arr, int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
 int main(){
     scanf("%d",&n);
     for(int i=0;i<n;i++) scanf("%d",&arr[i]);
