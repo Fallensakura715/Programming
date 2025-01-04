56fd33c5824148aad4b70eab01047edec534bc55
Fallensakura <fallensakura715@gmail.com>
2025-01-04 16:41:12 +0800

    Update hashTable.cpp

--- datastructure/hashTable.cpp
+++ datastructure/hashTable.cpp
@@ -39,3 +39,57 @@ int main() {
     }
     return 0;
 }
+
+// typedef struct HashNode{
+//     int  key;
+//     struct HashNode *next;
+// }HashNode,* HashTable;
+
+// void CreateHash(HashTable HT[], int n) {
+//     for (int i = 0; i < n; ++i) {
+//         int k;
+//         cin >> k;
+//         int idx = (k % P + P) % P;
+
+//         HashNode* node = new HashNode;
+//         node->key = k;
+//         node->next = nullptr;
+
+//         if (HT[idx] == nullptr) {
+//             HT[idx] = node;
+//         }
+//         else {
+//             HashNode* p = HT[idx];
+//             while (p->next != nullptr) {
+//                 p = p->next;
+//             }
+//             p->next = node;
+//         }
+//     }
+// }
+
+// 计算平均查找长度
+// 对于第 i 个非空桶，记该桶中链表长度为 c_i，成功查找其中任意元素时平均需要 (c_i + 1) / 2 次比较
+// float ASL(HashTable HT[]) {
+//     int elems = 0;
+//     float weightedSum = 0.0f;
+
+//     for (int i = 0; i < P; ++i) {
+//         int len = 0;
+//         HashNode* p = HT[i];
+//         while (p) {
+//             len++;
+//             p = p->next;
+//         }
+
+//         if (len > 0) {
+//             weightedSum += len * ((len + 1.0f) / 2.0f);
+//             elems += len;
+//         }
+//     }
+
+//     if (elems == 0) {
+//         return 0.0f;
+//     }
+//     return weightedSum / static_cast<float>(elems);
+// }
