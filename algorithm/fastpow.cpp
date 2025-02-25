4c087f851baa237c6322a31f6a25dbe1cb8d7237
Fallensakura <fallensakura715@gmail.com>
2025-02-25 18:51:08 +0800

    Update fastpow.cpp

--- algorithm/fastpow.cpp
+++ algorithm/fastpow.cpp
@@ -2,6 +2,14 @@
 
 using namespace std;
 
+/*
+    快速幂
+    同余性质
+    (a * b mod p) = (a mod p) * (b mod p) mod p;
+    若n是偶数，a ^ n = (a ^ (n / 2))^2 = (a * a) ^ (n >> 1);
+    若n是奇数, a ^ n = a * (a ^ (n - 1));
+*/
+
 long long pow(long long a, long long n) {
     long long result = 1;
     while (n > 0) {
